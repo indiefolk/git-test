a1b45fc8fad218e8ff8fbd51f184fcd94e4c32ae
John Lund <John.Lund@burberry.com>
2014-09-01 13:06:49 +0100

    Added User class

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ var data = {
 			name: 'John',
 			email: 'john.lund@burberry.com',
 			description: 'developer',
+			status: 'active'
 		}
 	]
 }
 
+
+var User = {
+
+	init: function () {
+
+		console.log('log');
+
+	}
+}
